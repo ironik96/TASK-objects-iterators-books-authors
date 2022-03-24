76139a945bc12e06e9fb4f24f1e41f38f605ec4c
Ahmed AlAmiri <a.alamiri.96@gmail.com>
2022-03-24 08:41:25 +0300

    Pass all test in functions.js file

--- functions.js
+++ functions.js
@@ -70,8 +70,17 @@ function booksByColor(books) {
  * - returns an array of the titles of the books written by that author:
  *    ["The Hitchhikers Guide", "The Meaning of Liff"]
  ****************************************************************/
+/**
+ * @param  {string} authorName
+ * @param  {Array} authors
+ * @param  {Array} books
+ */
 function titlesByAuthorName(authorName, authors, books) {
-  let bookIDs = authors.find((author) => author.name === authorName).books;
+  let getAuthors = authors.find(
+    (author) => author.name.toLowerCase() === authorName.toLowerCase()
+  );
+  if (getAuthors === undefined) return [];
+  let bookIDs = getAuthors.books;
   let authorBooks = books.filter((book) => bookIDs.includes(book.id));
   return authorBooks.map((book) => book.title);
 }
@@ -120,36 +129,55 @@ function mostProlificAuthor(authors) {
  *
  * BONUS: REMOVE DUPLICATE BOOKS
  ****************************************************************/
+/**
+ * @param  {string} bookId
+ * @param  {Array} authors
+ * @param  {Array} books
+ */
 function relatedBooks(bookId, authors, books) {
-  // get related authors ids
-  let bookAuthors = books.find((book) => book.id === bookId).authors;
-  let authorIDs = bookAuthors.map((author) => author.id);
-  // get related books ids
+  // get related authors id in an array
+  let relatedAuthors = getBookById(bookId, books).authors.map(
+    (author) => author.id
+  );
+  // map author id to book array (resulting in array of array)
+  let authorIDToBooksArray = authors
+    .filter((author) => relatedAuthors.includes(author.id))
+    .map((author) => author.books);
+  // flatten nested array
+  let relatedBooksIDs = [].concat.apply([], authorIDToBooksArray);
+  // find book title WITH duplicates
   let relatedBooks = [];
-  let relatedBooksIDs = [];
-  authorIDs.forEach((id) => {
-    let relatedAuthors = authors.filter((author) => author.id === id);
-    let relatedAuthorsBooksIDs = [];
-    relatedAuthors.forEach((author) => {
-      relatedAuthorsBooksIDs = new Set(relatedBooksIDs.concat(author.books));
-    });
-    relatedBooks = books
-      .filter((book) => relatedBookIDs.includes(book[id]))
-      .map((book) => book[title]);
+  relatedBooksIDs.forEach((id) => {
+    relatedBooks.push(books.find((book) => book.id === id).title);
   });
-  return;
-  return Array(relatedBooks);
+  return relatedBooks.sort();
 }
 // console.log(relatedBooks(50, authors, books));
-
 /**************************************************************
  * friendliestAuthor(authors):
  * - receives a list of authors
  * - returns the name of the author that has
  *   co-authored the greatest number of books
  ****************************************************************/
+/**
+ * @param  {Array} authors
+ */
 function friendliestAuthor(authors) {
-  // Your code goes here
+  // filter in books that are co authored
+  let coAuthoredBooks = books.filter((book) => book.authors.length > 1);
+  // map books to objects where the key value pair = name and count
+  let coAuthors = {};
+  coAuthoredBooks.forEach((book) => {
+    book.authors.forEach((author) => {
+      author.name in coAuthors
+        ? coAuthors[author.name]++
+        : (coAuthors[author.name] = 1);
+    });
+  });
+  // find the heighest count
+  let heighestCount = Math.max(...Object.values(coAuthors));
+  // return the name (key) of the heighest count
+  return Object.keys(coAuthors).find((key) => coAuthors[key] === heighestCount);
 }
 // console.log(friendliestAuthor(authors));
 
